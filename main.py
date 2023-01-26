4cd09f0c04fd4d8dc8fc98f1dca92364480a16be
Sergey Matofonov <phaeton.kz@gmail.com>
2023-01-27 00:18:44 +0300

    test buttons

--- main.py
+++ main.py
@@ -1,7 +1,59 @@
 from modules import vk_access
+import configparser
+from random import randrange
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+access_token = config['Tokens']['vk_token']
+group_token = config['Tokens']['group_token']
+user_id1 = config['Tokens']['user_id']
+vk = vk_api.VkApi(token=group_token)
+longpoll = VkLongPoll(vk)
+# Клавиатура
+keyboard = VkKeyboard(one_time=True)
+# keyboard = VkKeyboard(inline=True)  # Inline-режим
+keyboard.add_button('Найти людей', color=VkKeyboardColor.SECONDARY)
+keyboard.add_line()
+keyboard.add_button('Просмотр избранных', color=VkKeyboardColor.POSITIVE)
+keyboard.add_line()
+keyboard.add_button('Выход', color=VkKeyboardColor.NEGATIVE)
+vk_session = vk_api.VkApi(token=group_token)
+vk2 = vk_session.get_api()
+# Callback клавиатура
+# from vk_api.keyboard import VkKeyboardButton
+# keyboard_callback = VkKeyboardButton()
+# keyboard_callback.CALLBACK('Something')
 
 
+def write_msg(user_id, message):
+    vk.method('messages.send', {
+        'user_id': user_id,
+        'message': message,
+        'random_id': randrange(10 ** 7),
+    })
+
+
+def sender(user_id, text):
+    vk2.messages.send(user_id=user_id, message=text, random_id=0, keyboard=keyboard.get_keyboard())
 
 
 if __name__ == "__main__":
-    print(vk_access.vk.users_info())
+    vk1 = vk_access.VK(access_token, user_id1)
+    first_name = vk1.users_info()['response'][0]['first_name']  # Получаем имя пользователя
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+
+            if event.to_me:
+                request = event.text
+
+                if request == "привет":
+                    write_msg(event.user_id, f"Хай, {first_name}")
+                    sender(event.user_id, 'Какие планы на сегодня?')
+                elif request == "пока":
+                    write_msg(event.user_id, "До новых встреч)")
+                else:
+                    write_msg(event.user_id, "Не поняла вашего ответа...")
--- modules/vk_access.py
+++ modules/vk_access.py
@@ -1,22 +1,45 @@
 import requests
+# import vk_api
+from random import randrange
 import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
 
 class VK:
 
-   def __init__(self, access_token, user_id, version='5.131'):
-       self.token = access_token
-       self.id = user_id
-       self.version = version
-       self.params = {'access_token': self.token, 'v': self.version}
+    def __init__(self, access_token, user_id, version='5.131'):
+        self.token = access_token
+        self.id = user_id
+        self.version = version
+        self.params = {'access_token': self.token, 'v': self.version}
 
-   def users_info(self):
-       url = 'https://api.vk.com/method/users.get'
-       params = {'user_ids': self.id}
-       response = requests.get(url, params={**self.params, **params})
-       return response.json()
+    def users_info(self):
+        url = 'https://api.vk.com/method/users.get'
+        params = {'user_ids': self.id}
+        response = requests.get(url, params={**self.params, **params})
+        return response.json()
 
-
-access_token = 'vk1.a.CasC3sJWxqhRV7k54DcK-m76JdA25FdxKrj0661UxDZPeXLXBnR6i6hXR7vaNH3lIDJFWMEnLmChj41WJhK46kwl5CtraL6T4D4IzNO9SAmMU6EbyJzuW1dcWlNmxoU6oVQt3FxM7KLIAv-Hax9Y99Qzx89nb3XkOOjT15wQgcMqd-5H0vHx_N-fDGqvmEYps-NLJok4oKBU8DMif21ndQ'
-user_id = '179864387'
-vk = VK(access_token, user_id)
-# print(vk.users_info())
+#
+#
+# # token = input('Token: ')
+# token = 'vk1.a.CasC3sJWxqhRV7k54DcK-m76JdA25FdxKrj0661UxDZPeXLXBnR6i6hXR7vaNH3lIDJFWMEnLmChj41WJhK46kwl5CtraL6T4D4IzNO9SAmMU6EbyJzuW1dcWlNmxoU6oVQt3FxM7KLIAv-Hax9Y99Qzx89nb3XkOOjT15wQgcMqd-5H0vHx_N-fDGqvmEYps-NLJok4oKBU8DMif21ndQ'
+# vk = vk_api.VkApi(token=token)
+# longpoll = VkLongPoll(vk)
+#
+#
+# def write_msg(user_id, message):
+#     vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+#
+#
+# for event in longpoll.listen():
+#     if event.type == VkEventType.MESSAGE_NEW:
+#
+#         if event.to_me:
+#             request = event.text
+#
+#             if request == "привет":
+#                 write_msg(event.user_id, f"Хай, {event.user_id}")
+#             elif request == "пока":
+#                 write_msg(event.user_id, "Пока((")
+#             else:
+#                 write_msg(event.user_id, "Не поняла вашего ответа...")
